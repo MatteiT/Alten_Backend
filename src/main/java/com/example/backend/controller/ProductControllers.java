cd33516bf076a2a14e6a6be32ce1bcad099cb92a
MatteiT <107050929+MatteiT@users.noreply.github.com>
2023-09-27 15:52:25 +0200

    Adding Controllers for the asked endpoints

--- src/main/java/com/example/backend/controller/ProductControllers.java
+++ src/main/java/com/example/backend/controller/ProductControllers.java
@@ -1,4 +1,80 @@
 package com.example.backend.controller;
 
+import com.example.backend.entity.Product;
+import com.example.backend.repository.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
 public class ProductControllers {
+    private final ProductRepository productRepository;
+
+    public ProductControllers(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        return product.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product createProduct(@RequestBody @Valid Product product) {
+        return productRepository.save(product);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Product> updateProductPartial(@PathVariable Long id, @RequestBody @Valid Product updatedProduct) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            Product existingProduct = product.get();
+            if (updatedProduct.getName() != null) {
+                existingProduct.setName(updatedProduct.getName());
+            }
+            if (updatedProduct.getDescription() != null) {
+                existingProduct.setDescription(updatedProduct.getDescription());
+            }
+            productRepository.save(existingProduct);
+            return ResponseEntity.ok(existingProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody @Valid Product updatedProduct) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            updatedProduct.setId(id); // Ensure the ID remains the same
+            productRepository.save(updatedProduct);
+            return ResponseEntity.ok(updatedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()) {
+            productRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
