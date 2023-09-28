93ee5cf4606c6a6abd70d740b087894ac5f01fd2
MatteiT <107050929+MatteiT@users.noreply.github.com>
2023-09-28 14:49:53 +0200

    Initializer for the products.json at the start of the app with a Wrapper

---
+++ src/main/java/com/example/backend/config/DataInit.java
@@ -0,0 +1,36 @@
+package com.example.backend.config;
+
+import com.example.backend.entity.Product;
+import com.example.backend.repository.ProductRepository;
+import com.example.backend.service.ProductDataWrapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Component
+public class DataInit {
+
+    private final ProductRepository productRepository;
+
+    public DataInit(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            InputStream inputStream = getClass().getResourceAsStream("/json/products.json");
+            ProductDataWrapper wrapper = objectMapper.readValue(inputStream, ProductDataWrapper.class);
+            List<Product> products = wrapper.getData();
+            productRepository.saveAll(products);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
